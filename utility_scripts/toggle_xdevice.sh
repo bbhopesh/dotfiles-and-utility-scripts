87f5aa00eaefeb8e8314a0865d7366aee4e49e85
Bhopesh Bassi <bbhopesh@gmail.com>
2018-07-14 18:16:01 -0500

    Disable notifications while toggling xinput device.

--- utility_scripts/toggle_xdevice.sh
+++ utility_scripts/toggle_xdevice.sh
@@ -4,7 +4,9 @@ read TPdevice <<< $( xinput | sed -nre "/$1/s/.*id=([0-9]*).*/\1/p" )
 state=$( xinput list-props "$TPdevice" | grep "Device Enabled" | grep -o "[01]$" )
 
 if [ "$state" -eq '1' ];then
-    xinput --disable "$TPdevice" && notify-send -i emblem-nowrite "$1" "Disabled"
+    # xinput --disable "$TPdevice" && notify-send -i emblem-nowrite "$1" "Disabled"
+    xinput --disable "$TPdevice" 
 else
-    xinput --enable "$TPdevice" && notify-send -i input-touchpad "$1" "Enabled"
+    # xinput --enable "$TPdevice" && notify-send -i input-touchpad "$1" "Enabled"
+    xinput --enable "$TPdevice" 
 fi
