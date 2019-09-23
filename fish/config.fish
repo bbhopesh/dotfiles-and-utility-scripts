c5e0a67d2a7af9e92a9cedaf9eedfa658ce53dd9
Bhopesh Bassi <bbhopesh+github@pm.me>
2019-09-23 07:13:47 -0700

    bash and fish files.

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+fish_vi_key_bindings
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/bbassi/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
